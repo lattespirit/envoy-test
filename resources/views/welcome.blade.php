7ad58abbcfc4d83c562bdbd2a477e9823edf79fa
lattespirit <lattespirit@gmail.com>
2017-09-29 16:34:31 +0800

    change homepage text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Change Laravel
                 </div>
 
                 <div class="links">
